166cb43244d79758bdcde7c23e3044416a74c033
mukul <mukulbhardwaj966@gmail.com>
2024-04-09 17:50:09 +0530

    improve error handling logic

--- src/Login/ErrorMessage.js
+++ src/Login/ErrorMessage.js
@@ -2,9 +2,11 @@
 const ErrorMessages = {
     email: "Email is invalid",
     empty: "Can't be empty",
-    spacesNotAllowed: "Spaces are not allowed",
+    spacesNotAllowed: "Can have A-Z, 0-9, _, - Only",
     alreadyTaken: "has already been taken",
     passwordLengthError: "is too short(minimum is 6 characters)",
-    termsConditionError: "You must agree to the Terms of Service"
+    termsConditionError: "You must agree to the Terms of Service",
+
+
 };
 export default ErrorMessages;
