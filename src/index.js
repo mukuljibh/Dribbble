f98eacf7048317a8ee8f05d64cde88dcc146f109
mukul <mukulbhardwaj966@gmail.com>
2024-04-14 00:31:28 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import UserPreferencePage from './UserPreferencePage.jsx'
-import ProfileCreationPage from './ProfileCreationPage.jsx';
-import VerifyEmailPage from './EmailVerification/VerifyEmailPage.jsx';
-import SignupPage from './Signup/SignupPage.jsx';
+
 import App from './App.js';
 import store from "./reducers/store.js";
 import { Provider } from 'react-redux'
