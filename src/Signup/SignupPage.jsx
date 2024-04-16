98c6e1068d368274fd843589044550133a7cec57
mukul <mukulbhardwaj966@gmail.com>
2024-04-16 15:14:26 +0530

    changes some loading feature

--- src/Signup/SignupPage.jsx
+++ src/Signup/SignupPage.jsx
@@ -72,6 +72,7 @@ export default function SignupPage() {
           navigateTo("/profile");
         })
         .catch((err) => {
+          SetIsLoading(false);
           setAlreadyTaken(err);
         });
       dispatch(
